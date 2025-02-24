eaee22668002625951599d51798794f6f20dae3a
NakshtraNamahaCreations <opsnnc@gmail.com>
2025-02-24 18:31:06 +0530

    added the i to order id

--- src/controllers/leadController.js
+++ src/controllers/leadController.js
@@ -58,7 +58,7 @@ export const  createLead = async (req, res) => {
           ? parseInt(lastLead.orderId.replace("MMD2025", ""), 10) + 1
           : 1;
 
-      const orderId = `MMD2025${String(lastId).padStart(3, "1")}`;
+      const orderId = `MMD2025${String(lastId).padStart(4, "1")}`;
 
     
       const newLead = new Lead({ ...leadData, orderId });
